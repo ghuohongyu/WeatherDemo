4afc5a287423a676cb460339bdcde70642012c78
guohongyu <1070159376@qq.com>
2018-09-26 17:47:11 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/guohongyu/weatherdemo/MainActivity.java
@@ -0,0 +1,12 @@
+package cn.edu.pku.guohongyu.weatherdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
